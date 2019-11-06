b23f407bdce7d926c06e66207526b4faaa7879e6
geatrigger <geatrigger@gmail.com>
2019-11-06 20:00:29 +0900

    Add query, path processing

--- week5/index.js
+++ week5/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
 app.use((req, res, next) =>{
-    console.log(`Time: ${Date.now()}`);
+    bodyParser.urlencoded({extended: true});
     next();
 });
 
+app.get('/query', (req, res) => {
+    const message = Object.keys(req.query).map(k => `${k}: ${req.query[k]}`).join('\n');
+    res.send('GET /query\n' + message);
+});
+app.get('/board/:page', (req, res) => {
+    res.send('Board #' + req.params.page);
+});
 app.get('/', (req, res) => res.send('GET /'));
-app.get('/hi', (req, res) => res.send('hey'));
+app.post('/', (req, res) => res.send('POST /'));
+app.put('/', (req, res) => res.send('PUT /'));
+app.delete('/', (req, res) => res.send('DELETE /'));
+
 app.listen(port, () => console.log(`Week 5 practice server is working...`));
